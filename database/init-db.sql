a8b5e62bb033b1c01dc3b1e0495b163836ad73fa
Benoit.G <benoitgaugiran@gmail.com>
2025-09-07 15:25:33 +0200

    Hotfixed typo

--- database/init-db.sql
+++ database/init-db.sql
@@ -7,8 +7,8 @@ CREATE DATABASE tzktdb;
 DROP TABLE IF EXISTS delegations CASCADE;
 CREATE TABLE delegations (
     id SERIAL PRIMARY KEY,
-    adress VARCHAR(50) NOT NULL UNIQUE,
-    timestamp TIMESTAMP NOT NULL
-    amount BIGINT NOT NULL
+    address VARCHAR(50) NOT NULL UNIQUE,
+    timestamp TIMESTAMP NOT NULL,
+    amount BIGINT NOT NULL,
     blockheight BIGINT NOT NULL
 );
